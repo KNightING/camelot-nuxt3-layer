17b5109f671df7f2c076a404939b74e24bea7a20
unknown <keter_ciou@asus.com>
2024-02-21 15:19:31 +0800

    feat: update base api

--- composables/useBaseApi.ts
+++ composables/useBaseApi.ts
@@ -1,5 +1,7 @@
 import type { UseFetchOptions } from 'nuxt/app'
+import type { FetchContext } from 'ofetch'
 import { toValue } from '@vueuse/shared'
+import { resolveOptionsAndHash } from 'naive-ui/es/image/src/utils'
 
 export type Url =
   | string
@@ -187,3 +189,126 @@ export const useBaseApi = () => {
 //     return await new Promise<string>((resolve, reject) => { })
 //   }
 // }
+
+type OnRequest = (context: FetchContext) => Promise<void> | void;
+
+export const useDefaultHeaders = () => {
+  return reactive({
+    // 阻止瀏覽器探知檔案的 mime type
+    'X-Content-Type-Options': 'nosniff',
+    // 對 same-origin 的 URL 正常送出 Referer，但不對 cross-origin 送出
+    'Referrer-Policy': 'same-origin'
+  })
+}
+
+export const useBasicTokenRequest = (accountRef: MaybeRefOrGetter<string>, pwdRef: MaybeRefOrGetter<string>) => computed<OnRequest>(() => {
+  const account = toValue(accountRef)
+  const pwd = toValue(pwdRef)
+  const token = useBasicToken(account, pwd)
+  return ({ options }) => {
+    options.headers = { ...options.headers, Authorization: `Basic ${token}` }
+  }
+})
+
+const useBearerTokenRequest = (tokenRef: MaybeRefOrGetter<string>) => computed<OnRequest>(() => {
+  const token = toValue(tokenRef)
+  return ({ options }) => {
+    options.headers = { ...options.headers, Authorization: `Bearer ${token}` }
+  }
+})
+
+export type ApiFetchOptions<
+  DataT = any
+> = Omit<UseFetchOptions<DataT>, 'onRequest' | 'onRequestError' | 'onResponse' | 'onResponseError'> & {
+  contentType?: ContentType;
+  /**
+  * "default" 目前與 "clearNuxtData" 相同: 需先設定key
+  */
+  cachePolicy?: 'default' | 'clearNuxtData' | 'useNuxtData',
+  onRequests?: OnRequest[],
+};
+
+const useApiFetch = async <DataT>(
+  url: Url,
+  method: 'get' | 'post' | 'patch' | 'put' | 'delete',
+  options: ApiFetchOptions<DataT> = {
+    headers: useDefaultHeaders(),
+    cachePolicy: 'default'
+  }
+) => {
+  const statusCode = ref(0)
+  const { data, error, refresh, status, pending } = await useFetch(
+    url,
+    options && {
+      method,
+      getCachedData(key) {
+        if (options.cachePolicy === 'useNuxtData') {
+          if (useNuxtData(key).data.value) {
+            return useNuxtData(key)
+          }
+        }
+      },
+      async onRequest(context) {
+        if (options.cachePolicy === 'default' || options?.cachePolicy === 'clearNuxtData') {
+          clearNuxtData(options.key)
+        }
+
+        if (!context.options.headers) {
+          context.options.headers = {}
+        }
+
+        if (options.onRequests) {
+          for (const request of options.onRequests) {
+            await request(context)
+          }
+        }
+      },
+      onResponse(context) {
+        statusCode.value = context.response.status
+      },
+      onResponseError(context) {
+        statusCode.value = context.response.status
+      }
+    }
+  )
+  // when options set immediate is false, pending default is true, but status is idle
+  const isPending = computed(() => status.value === 'pending')
+  const isSuccess = computed(() => status.value === 'success')
+  const isError = computed(() => status.value === 'error')
+  return { data, error, refresh, status, pending, isSuccess, isError, isPending, statusCode }
+}
+
+const useGetFetch = <DataT>(
+  url: Url,
+  options?: ApiFetchOptions<DataT>
+) => {
+  return useApiFetch(url, 'get', options)
+}
+
+const usePostFetch = <DataT>(
+  url: Url,
+  options?: ApiFetchOptions<DataT>
+) => {
+  return useApiFetch(url, 'post', options)
+}
+
+const usePatchFetch = <DataT>(
+  url: Url,
+  options?: ApiFetchOptions<DataT>
+) => {
+  return useApiFetch(url, 'patch', options)
+}
+
+const usePutFetch = <DataT>(
+  url: Url,
+  options?: ApiFetchOptions<DataT>
+) => {
+  return useApiFetch(url, 'put', options)
+}
+
+const useDeleteFetch = <DataT>(
+  url: Url,
+  options?: ApiFetchOptions<DataT>
+) => {
+  return useApiFetch(url, 'delete', options)
+}
