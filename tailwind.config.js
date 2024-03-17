53991f630e216130c5897df5545b6f467dcbdce5
KNightING <l.xiang666@gmail.com>
2024-03-17 22:15:28 +0800

    chore: update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,vue,ts}',
+    './components/**/*.vue',
     './layouts/**/*.vue',
     './pages/**/*.vue',
-    './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
-    './app.vue'
+    './app.vue',
+    './error.vue'
   ],
   theme: {
     extend: {
