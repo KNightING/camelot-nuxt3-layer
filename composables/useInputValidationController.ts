5d6db9b5a5ea6094c16a1580ce08b72379b10019
KNightING <l.xiang666@gmail.com>
2024-04-24 21:25:21 +0800

    feat: update useInputValidationController

--- composables/useInputValidationController.ts
+++ composables/useInputValidationController.ts
@@ -1,12 +1,19 @@
 export type ValidatorFn = () => boolean
 
 export interface InputValidationController {
+  addValidatorComputed:(ref:ComputedRef<boolean>)=>void
   addValidator: (fn: ValidatorFn) => void
   validate: () => boolean
+  isValidate: ComputedRef<boolean>
 }
 
 export const useInputValidationController = ():InputValidationController => {
   const validateFnList = ref<(ValidatorFn)[]>([])
+  const validateRefList = ref<ComputedRef<boolean>[]>([])
+
+  const addValidatorComputed = (ref:ComputedRef<boolean>) => {
+    validateRefList.value.push(ref)
+  }
 
   const addValidator = (fn: ValidatorFn) => {
     validateFnList.value.push(fn)
@@ -24,8 +31,14 @@ export const useInputValidationController = ():InputValidationController => {
     // return validateFnList.value.every(fn => fn())
   }
 
+  const isValidate = computed(() => {
+    return validateRefList.value.every(ref => ref.value)
+  })
+
   return {
+    addValidatorComputed,
     addValidator,
-    validate
+    validate,
+    isValidate
   }
 }
