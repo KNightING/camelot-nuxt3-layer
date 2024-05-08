d71b5407584ca4fe0f2159ecf5312becba2cb389
KNightING <l.xiang666@gmail.com>
2024-05-08 21:53:04 +0800

    feat: add usePiniaClear

---
+++ composables/usePiniaClear.ts
@@ -0,0 +1,54 @@
+import { getActivePinia, type Pinia, type Store } from 'pinia'
+
+interface ExtendedPinia extends Pinia {
+  _s: Map<string, Store>;
+}
+export const usePiniaReset = (): Record<string | 'all', () => void> => {
+  const pinia = getActivePinia() as ExtendedPinia
+
+  if (!pinia) {
+    throw new Error('There is no stores')
+  }
+
+  const resetStores: Record<string, () => void> = {}
+
+  pinia._s.forEach((store, name) => {
+    try {
+      resetStores[name] = () => store.$reset()
+    } catch (e) {
+
+    }
+  })
+
+  resetStores.all = () => {
+    try {
+      pinia._s.forEach(store => store.$reset())
+    } catch (e) {}
+  }
+  return resetStores
+}
+
+export const usePiniaDispose = (): Record<string | 'all', () => void> => {
+  const pinia = getActivePinia() as ExtendedPinia
+
+  if (!pinia) {
+    throw new Error('There is no stores')
+  }
+
+  const disposeStores: Record<string, () => void> = {}
+
+  pinia._s.forEach((store, name) => {
+    try {
+      disposeStores[name] = () => store.$dispose()
+    } catch (e) {
+
+    }
+  })
+
+  disposeStores.all = () => {
+    try {
+      pinia._s.forEach(store => store.$dispose())
+    } catch (e) {}
+  }
+  return disposeStores
+}
