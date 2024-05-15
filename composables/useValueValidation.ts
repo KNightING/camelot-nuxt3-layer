737bbe23d8f17dc58260fb0606ed1b36bc177558
KNightING <l.xiang666@gmail.com>
2024-05-15 17:01:14 +0800

    feat: add useValueValidation

---
+++ composables/useValueValidation.ts
@@ -0,0 +1,27 @@
+export const useValueValidation = (
+  target: MaybeRef<string | undefined>,
+  options: {
+    type: 'email' | 'mobile' | 'number' | 'phone';
+    allowUndefined?: boolean;
+  }
+) =>
+  computed(() => {
+    const value = toValue(target)
+
+    if (!value) {
+      return options.allowUndefined ?? tryOnMounted
+    }
+
+    switch (options.type) {
+      case 'email':
+        return /^[\w\\-\\.]+@([\w\\-]+\.)+[\w\\-]{2,4}$/.test(value)
+      case 'mobile':
+        return /^[0-9]+$/.test(value)
+      case 'phone':
+        return /^0\d{8,9}$/.test(value)
+      case 'number':
+        return /^[0-9]+$/.test(value)
+    }
+
+    return true
+  })
