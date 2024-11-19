3b2400ed64b6f0832a674bff68c9b7430c8a2644
KNightING <l.xiang666@gmail.com>
2024-11-19 17:23:58 +0800

    feat: update lang

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,10 +1,9 @@
 export default defineI18nConfig(() => ({
   fallbackLocale: {
-    en: ['en-us'],
-    'zh-tw': ['en'],
-    'zh-hans': ['zh-ch', 'zh-hant', 'zh-tw'],
-    default: ['zh-tw']
+    'en': ['en-us'],
+    'zh-hans': ['zh-tw'],
+    'default': ['zh-tw'],
   },
   globalInjection: true,
-  legacy: false
+  legacy: false,
 }))
