054c07f1050d3bab70cd70e54f88558b18716b0b
KNightING <l.xiang666@gmail.com>
2025-03-19 10:33:09 +0800

    feat: add CamelotGpu

---
+++ components/Camelot/Gpu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="transform-gpu"
+    :class="{
+      'fix-ios-scrolling': !disabledFixIosScrolling,
+    }"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  disabledFixIosScrolling?: boolean
+}>()
+</script>
+
+<!-- https://juejin.cn/post/7085526308460953637#heading-0 -->
+<style scoped>
+.fix-ios-transform {
+  -webkit-transform: translate3d(0,0,0);
+  -moz-transform: translate3d(0,0,0);
+  -ms-transform: translate3d(0,0,0);
+  transform: translate3d(0,0,0);
+}
+
+.fix-ios-scrolling {
+  -webkit-overflow-scrolling: touch;
+}
+</style>
