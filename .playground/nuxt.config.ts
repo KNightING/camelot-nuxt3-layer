7b520e4e73f342523da6c9902105119ff50d87d1
KNightING <l.xiang666@gmail.com>
2025-08-20 14:40:58 +0800

    feat: update playground

--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath } from 'node:url'
+
 export default defineNuxtConfig({
   extends: ['..'],
 
@@ -14,4 +16,11 @@ export default defineNuxtConfig({
   },
 
   compatibilityDate: '2024-11-16',
+
+  eslint: {
+    config: {
+      // Use the generated ESLint config for lint root project as well
+      rootDir: fileURLToPath(new URL('..', import.meta.url)),
+    },
+  },
 })
